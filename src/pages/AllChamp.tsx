f7c6347aa8a31001a5caec28f1f0ab0103b31b46
Eperjesi Vencel <>
2026-03-01 11:46:47 +0100

    elakadás

--- src/pages/AllChamp.tsx
+++ src/pages/AllChamp.tsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
 import apiClient from "../apiClient";
+import type { Champ } from "../types/Champ";
 
-function AllChamp(){
+function AllChamp() {
     const [champs, setChamps] = useState<Array<Champ>>([]);
 
     useEffect(() => {
         apiClient
         .get("/champions")
-    };[]);
+        .then((response) => setChamps(response.data))
+        .catch((reason) => alert(reason));
+    }, []);
+
+    return (
+        <>
+            <h1>All Champions</h1>
+            {champs.map((c)=>)(
+                <p>
+                    {c.name} - {c.role}
+                </p>
+            ))}
+        </>
+    );
 }
 
 
